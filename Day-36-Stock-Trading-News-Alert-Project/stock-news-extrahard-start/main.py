4acd5b1959cb2a7f5ea3851bf22f1cdf1400a2f5
Jessie Wang <jessie.harada@yahoo.com>
2021-08-24 20:42:37 +1000

    day35-36

--- Day-36-Stock-Trading-News-Alert-Project/stock-news-extrahard-start/main.py
+++ Day-36-Stock-Trading-News-Alert-Project/stock-news-extrahard-start/main.py
@@ -1,11 +1,64 @@
+import requests
+import datetime as dt
+import os
+
 STOCK = "TSLA"
 COMPANY_NAME = "Tesla Inc"
 
 ## STEP 1: Use https://www.alphavantage.co
-# When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
+# When STOCK price increase/decreases by 5%
+# between yesterday and the day before yesterday then print("Get News").
+stock_endpoint = "https://www.alphavantage.co/query"
+stock_parameters = {
+    "function": "TIME_SERIES_DAILY",
+    "symbol": STOCK,
+    "apikey": "PZ2E7YWVTCFCX72G"
+}
 
-## STEP 2: Use https://newsapi.org
-# Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
+response = requests.get(url=stock_endpoint, params=stock_parameters)
+response.raise_for_status()
+data = response.json()["Time Series (Daily)"]
+data_list = [value for (key, value) in data.items()]
+# print(data_list)
+yesterday_closing_price = float(data_list[0]["4. close"])
+the_day_before_closing_price = float(data_list[1]["4. close"])
+# print(yesterday_closing_price, the_day_before_closing_price)
+difference_price = yesterday_closing_price - the_day_before_closing_price
+up_down = None
+if difference_price <= 0:
+    up_down = "🔻"
+else:
+    up_down = "🔺"
+
+difference = round(abs(difference_price) / yesterday_closing_price) * 100
+print(difference)
+
+# should be >= 5, this is for testing purposes
+if difference >= 0:
+    # print("Get news")
+
+    ## STEP 2: Use https://newsapi.org
+    # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME.
+    # 8ea8c686fca940cebf348295fe1c3dd0
+    news_endpoint = "https://newsapi.org/v2/everything"
+    today = dt.datetime.now().date()
+    news_parameters = {
+        "q": "tesla",
+        "from": today.strftime('%Y-%m-%d'),
+        "sortBy": "publishedAt",
+        "apiKey": "8ea8c686fca940cebf348295fe1c3dd0",
+        "language": "en"
+    }
+
+    response = requests.get(url=news_endpoint, params=news_parameters)
+    response.raise_for_status()
+    data=response.json()["articles"][:3]
+    formatted_articles = [f"{STOCK}: {up_down}{difference}% \nHeadline: {article['title']} \nBrief: {article['description']}"for article in data]
+    print(formatted_articles)
+
+
+
+
 
 ## STEP 3: Use https://www.twilio.com
 # Send a seperate message with the percentage change and each article's title and description to your phone number. 
@@ -23,3 +76,7 @@ Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?.
 Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
 """
 
+
+# today = dt.datetime.now().date() - dt.timedelta(3)
+# yesterday = today - dt.timedelta(1)
+# today.strftime('%Y-%m-%d')
---
+++ Day-37-Habit-Tracking-Project-API-Post-Requests-Headers/main.py
@@ -0,0 +1,74 @@
+import requests
+from datetime import datetime
+
+# HTTP requests
+# GET: requests.get()
+# POST
+# PUT
+# DELETE
+
+# https://pixe.la/
+# https://docs.pixe.la/
+
+USERNAME = "jessie16"
+TOKEN = "hfshjkf3783jek"
+GRAPH_ID = "graph1"
+
+pixela_endpoint = "https://pixe.la/v1/users"
+user_params = {
+    "token":TOKEN,
+    "username":USERNAME,
+    "agreeTermsOfService":"yes",
+    "notMinor":"yes"
+}
+# set up user account
+# response = requests.post(url=pixela_endpoint, json=user_params)
+# print(response.text)
+
+# set up graph
+pixela_graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+# headers:
+graph_header = {
+    "X-USER-TOKEN": TOKEN
+}
+# query:
+graph_params= {
+    "id":GRAPH_ID,
+    "name":"movie time",
+    "unit":"minutes",
+    "type":"int",
+    "color":"sora"
+}
+# response = requests.post(url=pixela_graph_endpoint, json=graph_params, headers=graph_header)
+# print(response.text)
+
+
+# actual graph: https://pixe.la/v1/users/jessie16/graphs/graph1
+
+# https://pixe.la/v1/users/a-know/graphs/test-graph
+# $ curl -X POST https://pixe.la/v1/users/a-know/graphs/test-graph -H 'X-USER-TOKEN:thisissecret' -d '{"date":"20180915","quantity":"5"}'
+# {"message":"Success.","isSuccess":true}
+pixela_value_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
+# https://www.w3schools.com/python/python_datetime.asp
+today = datetime.now().strftime("%Y%m%d")
+# update a random day
+# random_day = datetime(year=2021, month= 7, day=23).strftime("%Y%m%d")
+value_params = {
+    "date": today,
+    "quantity": "30"
+}
+# response = requests.post(url=pixela_value_endpoint, json=value_params, headers= graph_header)
+# print(response.text)
+
+# put = update requests
+# $ curl -X PUT https://pixe.la/v1/users/a-know/graphs/test-graph -H 'X-USER-TOKEN:thisissecret' -d '{"name":"graph-name","unit":"commit","color":"shibafu","timezone":"Asia/Tokyo","purgeCacheURLs":["https://camo.githubusercontent.com/xxx/xxxx"],"publishOptionalData":true}'
+# {"message":"Success.","isSuccess":true}
+pixela_pixel_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{today}"
+update_values_params = {
+    "quantity": "50",
+}
+response = requests.put(url=pixela_pixel_endpoint, json=update_values_params, headers= graph_header)
+print(response.text)
+
+# delete requests
+# response_delete = requests.put(url=pixela_pixel_endpoint, headers=graph_header)
